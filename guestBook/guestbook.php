4039fd1136d8eefe3501db82d5d6438cb09743ae
R Sterken <ronniesterken19@gmail.com>
2018-02-22 18:23:50 +0100

    Added files
    added the files to the project

---
+++ guestBook/guestbook.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Guess book</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div class="main-container">
+      <div id="form-container">
+        <form>
+          <input type="text" name="firstNameField" placeholder="First name" required />
+          <input type="email" name="emailField" placeholder="E-mail" required />
+          <input type="text" name="websiteField" placeholder="website" />
+          <textarea placeholder="message" required></textarea>
+          <input type="button" name="Send" value="Send">
+          <input type="button" name="Reset" value="Reset inputs">
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
